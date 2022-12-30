1a35db9d0d1a46c7a4310fde21abbc1f236d15cf
Amirhosein-JVD <102900476+Amirhosein-JVD@users.noreply.github.com>
2022-12-30 23:25:06 +0330

    update

--- src/CircularGraph.py
+++ src/CircularGraph.py
@@ -1,4 +1,4 @@
-from tkinter import*
+from tkinter import *
 
 import math
 
@@ -10,66 +10,66 @@ root.geometry("900x600")
 
 root.title("Josephus problem")
 
-c = Canvas(root,width=600,height=600)
+c = Canvas(root, width=600, height=600)
 
 c.pack()
 
 root.withdraw()
 
-def create_circle(x,y,r,canvasName):
-    
-    x0 = x-r
-    y0 = y-r
-    x1 = x+r
-    y1 = y+r
 
-    return canvasName.create_oval(x0,y0,x1,y1,fill='orange',width=1.5)
+def create_circle(x, y, r, canvasName):
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
 
-#x0=300 y0=300 R=250
+    return canvasName.create_oval(x0, y0, x1, y1, fill='orange', width=1.5)
+
+
+# x0=300 y0=300 R=250
 
 def drawNodes(arr):
+    n = len(arr)
 
-    n=len(arr)
+    nodeRadius = int(1571 / (2 * n))
 
-    nodeRadius = int(1571/(2*n))
+    x = 550
 
-    x=550
+    y = 300
 
-    y=300
+    nodeAngel = math.pi * 2 / n
 
-    nodeAngel = (math.pi)*2/n
+    for i in range(1, n + 1):
 
-    for i in range(1,n+1):
+        if nodeRadius > 46:
 
-        if(nodeRadius>46):nodeRadius=46
+            nodeRadius = 46
 
-        if(n==1):
+        if n == 1:
+            x = 300
 
-            x=300
+            y = 300
 
-            y=300
+        create_circle(x, y, nodeRadius, c)
 
-        create_circle(x,y,nodeRadius,c)
+        c.create_text(x, y, text=arr[i - 1], font=('Helvetica', nodeRadius, 'bold'))
 
-        c.create_text(x,y,text=arr[i-1],font=('Helvetica',nodeRadius,'bold'))
+        x = 300 + 250 * (math.cos(i * nodeAngel))
 
-        x=300+250*(math.cos(i*nodeAngel))
+        y = 300 + 250 * (math.sin(i * nodeAngel))
 
-        y=300+250*(math.sin(i*nodeAngel))
 
 def display(mainArr):
-
     root.deiconify()
 
     for i in mainArr:
-
         drawNodes(i)
-
-        time.sleep(2)
 
         c.update()
 
         c.delete('all')
+
+        time.sleep(1)
 
     drawNodes(mainArr[-1])
 
